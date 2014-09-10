5889f807b02dcbf1bdd86336d2edc32a5b2489ae
Owner <Owner@10.30.20.79>
2014-09-10 17:18:21 -0500

    Initial Commit

---
+++ src/lab1/Course.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author Ching
+ */
+public class Course {
+  
+    public class AdvancedJavaCourse extends Course {
+
+
+    public AdvancedJavaCourse(String courseName, String courseNumber) {
+ 
+    }
+
+    public String getCapitalizedCourseName() {
+    
+    }
+
+    public String getPrerequisites() {
+    
+    }
+
+    public void setPrerequisites(String prerequisites) {
+
+    }
+
+    public void setCredits(double credits) {
+
+    }
+
+    public String getCourseName() {
+
+    }
+
+    public void setCourseName(String courseName) {
+
+    }
+
+    public String getCourseNumber() {
+
+    }
+
+    public void setCourseNumber(String courseNumber) {
+
+    }
+
+    public double getCredits() {
+
+    }
+    
+}
